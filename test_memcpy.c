1eaa7540bf3dbac68e93ec8ef292c66638ad5666
vonzhou <vonzhou@163.com>
2014-05-25 15:03:42 +0800

    test line

--- test_memcpy.c
+++ test_memcpy.c
@@ -4,8 +4,10 @@
 int main(void){
 	char tmp[20];
 	char *str = "vonzhou\n";
-	memcpy((void *)tmp,str,8+1);// remember to include the tailing  '\0'
+	memcpy((void *)tmp,str, strlen(str) + 1);// remember to include the tailing  '\0'
 	printf("%s",tmp);
 	
 	return 0;
 }
+
+
