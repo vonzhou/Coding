82133b67d2f52dad2702769360fd21d152f7e54d
vonzhou <vonzhou@163.com>
2014-10-08 16:11:01 +0800

    TLPI

---
+++ tlpi/55filelock/curr_time.h
@@ -0,0 +1,9 @@
+
+/* Header file for Listing 10-2 */
+
+#ifndef CURR_TIME_H
+#define CURR_TIME_H             /* Prevent accidental double inclusion */
+
+char *currTime(const char *fmt);
+
+#endif
---
+++ tlpi/README.txt
@@ -0,0 +1,2 @@
+
+学习 The Linux Programming Interface 这本书
