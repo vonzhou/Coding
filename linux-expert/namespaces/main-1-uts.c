1e27d18e1cfe111bf4368654a41dc5f6a2524f16
vonzhou <vonzhou@vonzhou.(none)>
2014-12-24 16:49:25 +0800

    linux namespace UTS

---
+++ linux-expert/namespaces/main-1-uts.c
@@ -0,0 +1,31 @@
+
+#define _GNU_SOURCE
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdio.h>
+#include<sched.h>
+#include<signal.h>
+#include<unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+static char child_stack[STACK_SIZE];
+char* const child_args[] = {
+    "/bin/bash",
+    NULL
+    };
+int child_main(void *arg){
+    printf(" - World!\n");
+    sethostname("In Namespace", 12);
+    execv(child_args[0], child_args);
+    printf("Ooops\n");
+    return 1;
+}
+
+int main(){
+    printf(" - Hello ?\n");
+    int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWUTS | SIGCHLD, NULL);
+    waitpid(child_pid, NULL, 0);
+    return 0;
+}
+
