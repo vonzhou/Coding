eed66b7859fabc86d7008ee034849b9d9a362449
vonzhou <vonzhou@vonzhou.(none)>
2015-01-04 20:54:30 +0800

    Communicate between userspace and kernel

--- kern/nelink/echo/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vonzhou/GitHub/Coding/kern/nelink/echo/built-in.o :=  rm -f /home/vonzhou/GitHub/Coding/kern/nelink/echo/built-in.o; ar rcsD /home/vonzhou/GitHub/Coding/kern/nelink/echo/built-in.o
--- kern/nelink/echo/.tmp_versions/nl_hello.mod
+++
@@ -1,2 +0,0 @@
-/home/vonzhou/GitHub/Coding/kern/nelink/echo/nl_hello.ko
-/home/vonzhou/GitHub/Coding/kern/nelink/echo/nl_hello.o
