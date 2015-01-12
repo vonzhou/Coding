6c785e423849ca75017aee0cc1948a3a6eba9858
vonzhou <vonzhou@163.com>
2015-01-12 08:22:49 -0500

    high low mem

---
+++ kern/high-mem/high_low.c
@@ -0,0 +1,62 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/highmem.h>
+#include <linux/gfp.h>
+#include <linux/mm_types.h>
+#include <asm/io.h>
+#include <linux/types.h>
+
+
+static int hello_init(void) {
+	// 内核中分配内存的常见情况
+	struct page *mypage = alloc_page(GFP_USER);
+	struct page *highmem_page;
+	if (!mypage) printk(KERN_ERR "Couldn't allocate highmem page with order : 3\n" );
+  	else {
+
+  		unsigned long page_addr = page_address(highmem_page);
+  		//printk(KERN_ERR "Address %lu\n", page_addr);
+     			
+
+     		phys_addr_t phy_addr = virt_to_phys(page_addr);
+  			printk(KERN_ERR "Address %lx\n", page_addr);
+     		printk(KERN_ERR "Physical Address is %lx \n", phy_addr);
+  	}
+
+  	//kunmap(highmem_page);
+  	__free_page( mypage);
+
+	// 分配高端内存 然后映射到内核空间
+  	highmem_page = alloc_page(GFP_HIGHUSER);
+	if (!highmem_page) printk(KERN_ERR "Couldn't allocate highmem page with order : 3\n" );
+  	else {
+
+  		long highmem_page_address = kmap(highmem_page);
+		phys_addr_t phy_addr;
+    		if (!highmem_page_address) printk(KERN_ERR "Couldn't map highmem pages.\n");
+    		else {
+      			printk(KERN_ERR "Address for highuser pages is : %lx. Order: %d.\n", 
+            			highmem_page_address,
+            			0
+           		);
+     		}
+
+     		phy_addr = virt_to_phys(page_address(highmem_page));
+     		printk(KERN_ERR "Physical Address is %lx \n", phy_addr);
+  	}
+
+  	kunmap(highmem_page);
+  	__free_page( highmem_page);
+
+  	return 0;
+}
+
+static void hello_exit(void) {
+  printk("<1> Bye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
