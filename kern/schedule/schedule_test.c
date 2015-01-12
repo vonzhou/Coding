5eb8df6cab0b56adb1d836138dd07766493214b1
vonzhou <vonzhou@163.com>
2015-01-12 02:18:59 -0500

    work

--- kern/schedule/schedule_test.c
+++ kern/schedule/schedule_test.c
@@ -2,20 +2,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/net.h>
-#include <net/sock.h>
-#include <linux/in.h>
-#include <linux/types.h>
-#include <linux/kthread.h>
-#include <linux/wait.h>
-#include <linux/skbuff.h>
-#include <linux/string.h>
-#include <asm-generic/unaligned.h>
-#include <linux/sysctl.h>
-#include <linux/netfilter.h>
-#include <linux/netfilter_ipv4.h>
-#include <asm/checksum.h>
-#include <linux/ip.h>
 #include <linux/workqueue.h>
 
 #define TEST_INTERVAL 10 * HZ
