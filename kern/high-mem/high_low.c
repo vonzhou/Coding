cc81164f922b779a168e9e6167c23ff66c62bb5a
vonzhou <vonzhou@163.com>
2015-01-12 22:19:14 -0500

    high mem

--- kern/high-mem/high_low.c
+++ kern/high-mem/high_low.c
@@ -9,51 +9,49 @@
 
 
 static int hello_init(void) {
-	// 内核中分配内存的常见情况
-	struct page *mypage = alloc_page(GFP_USER);
-	struct page *highmem_page;
-	if (!mypage) printk(KERN_ERR "Couldn't allocate highmem page with order : 3\n" );
-  	else {
+		// 内核中分配内存的常见情况
+		struct page *mypage = alloc_page(GFP_USER);
+		struct page *highmem_page;
+		if (!mypage) printk(KERN_ERR "Couldn't allocate normal page\n" );
+		else {
 
-  		unsigned long page_addr = page_address(highmem_page);
-  		//printk(KERN_ERR "Address %lu\n", page_addr);
-     			
+				unsigned long page_addr = page_address(mypage);
+				//printk(KERN_ERR "Address %lu\n", page_addr);
 
-     		phys_addr_t phy_addr = virt_to_phys(page_addr);
-  			printk(KERN_ERR "Address %lx\n", page_addr);
-     		printk(KERN_ERR "Physical Address is %lx \n", phy_addr);
-  	}
 
-  	//kunmap(highmem_page);
-  	__free_page( mypage);
+				phys_addr_t phy_addr = virt_to_phys(page_addr);
+				printk(KERN_ERR "Address %lx\n", page_addr);
+				printk(KERN_ERR "Physical Address is %lx \n", phy_addr);
+		}
 
-	// 分配高端内存 然后映射到内核空间
-  	highmem_page = alloc_page(GFP_HIGHUSER);
-	if (!highmem_page) printk(KERN_ERR "Couldn't allocate highmem page with order : 3\n" );
-  	else {
+		//kunmap(highmem_page);
+		__free_page( mypage);
 
-  		long highmem_page_address = kmap(highmem_page);
-		phys_addr_t phy_addr;
-    		if (!highmem_page_address) printk(KERN_ERR "Couldn't map highmem pages.\n");
-    		else {
-      			printk(KERN_ERR "Address for highuser pages is : %lx. Order: %d.\n", 
-            			highmem_page_address,
-            			0
-           		);
-     		}
+		// 分配高端内存 然后映射到内核空间
+		highmem_page = alloc_page(GFP_HIGHUSER);
+		if (!highmem_page) printk(KERN_ERR "Couldn't allocate highmem page \n" );
+		else {
 
-     		phy_addr = virt_to_phys(page_address(highmem_page));
-     		printk(KERN_ERR "Physical Address is %lx \n", phy_addr);
-  	}
+				unsigned long highmem_page_address = kmap(highmem_page);
+				phys_addr_t phy_addr;
+				if (!highmem_page_address) printk(KERN_ERR "Couldn't map highmem pages.\n");
+				else {
+						printk(KERN_ERR "Address for highuser pages after kmap is : %lx\n", 
+										highmem_page_address );
+				}
 
-  	kunmap(highmem_page);
-  	__free_page( highmem_page);
+				phy_addr = virt_to_phys(page_address(highmem_page));
+				printk(KERN_ERR "Physical Address is %lx \n", phy_addr);
+		}
 
-  	return 0;
+		kunmap(highmem_page);
+		__free_page( highmem_page);
+
+		return 0;
 }
 
 static void hello_exit(void) {
-  printk("<1> Bye, cruel world\n");
+		printk("====Bye===\n");
 }
 
 module_init(hello_init);
